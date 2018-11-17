99612fd90463b077ed5e40084ef41b88dc49f904
vberihuete <vincentberihuete809@gmail.com>
2018-11-17 08:32:36 -0400

    Initial Commit

---
+++ ToastView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToastView
+//
+//  Created by Vincent Berihuete on 11/17/18.
+//  Copyright © 2018 devcorerd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
